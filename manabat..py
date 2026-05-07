3253c8158aab666bcd5e707c387e34cffc03520c
namanabat <namanabat@cvisc.pshs.edu.ph>
2026-05-07 12:20:08 +0800

    Add files via upload

---
+++ manabat..py
@@ -0,0 +1,9 @@
+# import math
+g = 9.8
+
+a = int(input("Enter the launch angle(degrees):"))
+d = int(input("Enter the maximum distance(meters:"))
+v = math.sqrt((d*g)/(math.sin(2*(math.radians(a)))))
+print(v)Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Start small. Ship something.")
---
+++ manabat.py..py
@@ -0,0 +1,23 @@
+# Online Python compiler (interpretstudents_list = []  # List to hold student dictionaries
+
+# Loop 3 times to gather information for 3 students
+for i in range(3):
+    # Asking for name, age, and grade for each student
+    name = input("Enter the student's name: ")
+    age = input("Enter the student's age: ")
+    grade = input("Enter the student's grade: ")
+
+    # Create a dictionary with student's information
+    student_info = {
+        "name": name,
+        "age": age,
+        "grade": grade
+    }
+
+    # Add the student's dictionary to the list
+    students_list.append(student_info)
+
+# Print out the list of student dictionaries
+
+# Write Python 3 code in this online editor and run it.
+print("Start small. Ship something.")
